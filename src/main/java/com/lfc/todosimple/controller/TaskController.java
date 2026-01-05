ea28823c6c3c636f1c0fda379dcb79045a137525
Leo-FC <leonardo.ferreiracontato1@gmail.com>
2026-01-05 19:31:25 -0300

    refactor: TaskController com UserService

--- src/main/java/com/lfc/todosimple/controller/TaskController.java
+++ src/main/java/com/lfc/todosimple/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.lfc.todosimple.controller;
 
 import com.lfc.todosimple.model.Task;
 import com.lfc.todosimple.service.TaskService;
+import com.lfc.todosimple.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -21,6 +22,9 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<Task> findById(@PathVariable Long id){
         Task obj = this.taskService.findById(id);
@@ -29,6 +33,7 @@ public class TaskController {
 
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId){
+        userService.findById(userId);
         List<Task> objs = this.taskService.findAllByUserId(userId);
         return ResponseEntity.ok().body(objs);
     }
