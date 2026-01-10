954e047214366cc4487cd148bc7d611d500d182a
Leo-FC <leonardo.ferreiracontato1@gmail.com>
2026-01-10 17:29:27 -0300

    feat: JWTAuthenticationFilter

--- src/main/java/com/lfc/todosimple/config/SecurityConfig.java
+++ src/main/java/com/lfc/todosimple/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.lfc.todosimple.config;
 
+import com.lfc.todosimple.security.JWTAuthenticationFilter;
 import com.lfc.todosimple.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -58,7 +59,10 @@ public class SecurityConfig {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated().and()
+                .authenticationManager(authenticationManager);
+
+        http.addFilter(new JWTAuthenticationFilter(this.authenticationManager, this.jwtUtil));
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
